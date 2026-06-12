065cddcfbdc70389c2520518b5d480a7d158bcca
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-12 21:10:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        int n=x,rem=0;
+        long long sum=0;
+        while(n>0){
+            rem=n%10;
+            sum=sum*10+rem;
+            n=n/10;
+        }
+        if(sum==x)return true;
+        return false;
+    }
+};
