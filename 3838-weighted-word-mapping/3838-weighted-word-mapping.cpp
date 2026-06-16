8961fdbaa562c8d88137d80307cdadada6a90498
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-16 21:21:40 +0530

    Time: 3 ms (27.25%), Space: 45.7 MB (5.24%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -3,16 +3,12 @@ public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         char c;
         int sum;
-        map<char,int> mpp;
         string ans;
-        for(int i=0;i<26;i++){
-            mpp[97+i]=weights[i];
-        }
         for(auto i:words){
             sum=0;
             c='z';
             for(int j=0;j<i.size();j++){
-                sum+=mpp[i[j]];
+                sum+=weights[i[j]-'a'];
             }
             sum=sum%26;
             c=c-sum;
