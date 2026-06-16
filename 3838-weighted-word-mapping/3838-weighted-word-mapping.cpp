bab15b3ee8fb7db2f8b750621c75aa226e69da07
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-16 21:28:11 +0530

    Time: 0 ms (100%), Space: 43.8 MB (17.07%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -4,13 +4,14 @@ public:
         char s;
         int sum;
         string ans;
+        ans.reserve(words.size());
         for(auto i:words){
             sum=0;
             for(auto c:i){
                 sum+=weights[c-'a'];
             }
             s='z'- (sum%26);
-            ans=ans+s;
+            ans.push_back(s);
         }
         return ans;
     }
