219f622c69f37a539a178752fdf8e8349e310077
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-28 22:52:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (69.87%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int n1,n2;
-        for(int i=n;i>0;i--){
-            n1=(n*(n+1))/2;
-            n1+=i;
-            if(n1%2==1)continue;
-            n1=n1/2;
-            n2=((i+1)*i)/2;
-            if(n1==n2)return i;
-        }
-        return -1;
+        int pivot,sum=(n*(n+1))/2;
+        pivot=sqrt(sum);
+        return pivot*pivot==sum?pivot:-1;
     }
 };
