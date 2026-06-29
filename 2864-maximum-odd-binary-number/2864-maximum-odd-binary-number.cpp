7720d4bae19b44f713de4f55529afd00c1822d93
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-29 22:15:11 +0530

    Time: 3 ms (23.98%), Space: 9.8 MB (17.5%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int cnt=0,n=size(s);
+        for(auto c:s){
+            if(c=='1')cnt++;
+        }
+        int x=n-cnt;
+        string ans=string(cnt-1,'1')+string(x,'0')+"1";  
+        return ans;      
+    }
+};
