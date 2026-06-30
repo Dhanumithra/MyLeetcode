4ae1ba8ad40de9a3ac1032ff0f617846ba8739f5
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-30 22:13:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.07%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int win=0;
+        for(int i=2;i<=n;i++){
+            win=(win+k)%i;
+        }
+        return win+1;
+    }
+};
