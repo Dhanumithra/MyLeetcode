9583e3d35a4d614997c76662a9bf104d9cebbfc7
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-06-30 22:52:16 +0530

    Time: 0 ms (100%), Space: 28.5 MB (78.46%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>9)a+=nums[i];
+            else b+=nums[i];
+        }
+        if(a!=b)return true;
+        return false;
+    }
+};
