8f8149f303a87621edfe1b2a359c7e1699ee2c55
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-07-11 00:00:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.3%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        double dl,db;
-        int b,l,B,L;
-        int diff = INT_MAX;
-        for(l=1;l<=area;l++){
-            dl=l;
-            db=area/dl;
-            b=area/l;
-            if(db==b && diff>abs(l-b)){
-                diff=abs(l-b);
-                L=l;
-                B=b;
-            }
+        for(int s=sqrt(area);s>0;s--){
+            if(area%s==0)return {area/s,s};
         }
-        if(B>L){
-            swap(L,B);
-        }
-        return {L,B};
+        return {};
     }
 };
