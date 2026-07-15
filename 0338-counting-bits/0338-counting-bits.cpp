fe9188cabdfbd0c0bce2c33d6f45fea19561a9bd
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-07-15 19:51:52 +0530

    Time: 3 ms (23.37%), Space: 10.7 MB (56.89%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
+        vector<int>ans(n+1);
         int cnt=0,num;
         for(int i=0;i<n+1;i++){
             num=i;
@@ -10,7 +10,7 @@ public:
                 if(num%2==1)cnt++;
                 num/=2;
             }
-            ans.push_back(cnt);
+            ans[i]=cnt;
         }
         return ans;
     }
