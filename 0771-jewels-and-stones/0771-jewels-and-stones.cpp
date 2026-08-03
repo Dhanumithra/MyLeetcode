8fd6e96abd872e749d382b4b1f0a0cbbe29ac7e6
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-08-04 00:55:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (91.97%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int cnt=0;
+        for(char j:jewels){
+            cnt+=count(stones.begin(),stones.end(),j);
+        }
+        return cnt;
+    }
+};
