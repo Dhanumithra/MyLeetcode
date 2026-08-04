ab679964ccf223bd8f61764f9331a4128f6ff8f7
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-08-04 15:39:50 +0530

    Time: 0 ms (100%), Space: 31.3 MB (82.35%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0,mid,high=nums.size()-1;
+        while(low<=high){
+            mid=low+high;
+            mid/=2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
+    }
+};
