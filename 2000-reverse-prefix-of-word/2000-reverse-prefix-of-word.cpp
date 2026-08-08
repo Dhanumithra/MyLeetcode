facc1921ef84b7b5272edd2ce42ba52157a08be1
DHANUMITHRA T <dhanumithra6002@gmail.com>
2026-08-09 00:50:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (90.84%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int j=0;
+        for(int i=0;i<word.length();i++){
+            if(word[i]==ch){
+                while(j<=i){
+                    swap(word[i],word[j]);
+                    i--;
+                    j++;
+                }
+                return word;
+            }
+        }
+        return word;
+    }
+};
